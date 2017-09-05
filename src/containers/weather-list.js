92d1ffc2c168e02272e19492b9923c57d4f23e93
jl <jl@axin.fr>
2017-09-05 14:04:40 +0200

    creation composant reutilisable

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -1,15 +1,32 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
+import Chart from '../components/chart';
+
 class WeatherList extends Component{
 
     renderWeather (cityData){
        
         const cityName = cityData.city.name;
         
+        // -273 convert to celsius
+        const temps = cityData.list.map(weather => weather.main.temp - 273);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidity = cityData.list.map(weather => weather.main.humidity);
+
+
         return(
             <tr key={cityName} >
                 <td>{cityName}</td>
+                <td>
+                    <Chart color="orange" data={temps} units="°"/>
+                </td>
+                <td>
+                    <Chart color="green" data={pressures} units="hPa"/>
+                </td>
+                <td>
+                    <Chart color="blue" data={humidity} units="%"/>
+                </td>
             </tr>
        );
     }
@@ -20,9 +37,9 @@ class WeatherList extends Component{
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (°)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
